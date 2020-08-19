8a04a300021a3449be36e9fc67b6c07e203a8683
DCartist <pbannerman13@gmail.com>
2020-08-18 20:43:16 -0400

    adding script

---
+++ script.js
@@ -0,0 +1,4 @@
+let drinks = [{name:"Old Fashioned" ,image: "img.jpg", style: "Stirred", ingredients: "Sugar, water, bitters, whiskey"}]
+
+let cards = document.querySelectorAll(".card")
+console.log(cards)
